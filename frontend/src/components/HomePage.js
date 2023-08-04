68e82b4bc2b0bc61ade9e5c3e12947f4966bc25a
paulosvel <paul2001vel@gmail.com>
2023-08-04 15:09:34 +0300

    changes on routes

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -12,7 +12,7 @@ function HomePage() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<p>This is the homepage</p>} />
+        <Route path="/" element={<p>This is the homepage</p>} />
         <Route path="/join" element={<RoomJoinPage />} />
         <Route path="/create" element={<CreateRoomPage />} />
       </Routes>
